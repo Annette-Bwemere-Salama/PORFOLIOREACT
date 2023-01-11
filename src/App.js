2608b22acfcf7858e89dd8c65cd25a02d0b0af6a
Annette Bwemere <bwemereannette@gmail.com>
2023-01-11 15:38:03 +0200

    feat: refactory

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import React from "react";
 
 import "./App.css";
 
-import Header from "./Composants/Header.js";
-
-// import Menu from "./Composants/Menu.js";
-
-import Main from "./Composants/Main.js";
-
-import Footer from "./Composants/Footer.js";
-
 
 function App() {
   return (
     <div>
       {/* <Menu/> */}
       < div>
-        <Header />
-        <Main />
-        <Footer />
+<h1>My porfolio</h1>
       </div>
     </div>
 
