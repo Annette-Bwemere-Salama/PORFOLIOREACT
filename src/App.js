6375ae03a162b6464247df1cf054cf052ed42d57
Annette Bwemere <bwemereannette@gmail.com>
2023-01-11 22:16:28 +0200

    feat arrangemtn style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
-
-import "./App.css";
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Home from './pages/Home';
+import Experience from './pages/Experience';
+import Projects from './pages/Projects';
+import Navbar from './component/Navbar';
 
 function App() {
-  return (
-    <div>
-      {/* <Menu/> */}
-      < div>
-<h1>My porfolio</h1>
-      </div>
-    </div>
 
-  )
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+
 }
 
-export default App
+export default App;
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import "../styles/navbar.css"
-import Home from '../pages/Home'
-import Projects from '../pages/Projects'
-import Experience from '../pages/Experience'
 function Navbar() {
   return (
     <div className='navbar'>
@@ -11,9 +8,9 @@ function Navbar() {
             <button></button>
         </div>
         <div className='links'>
-            <Link to="/">{Home}</Link>
-            <Link to='/project'>{Projects={Projects}}</Link>
-            <Link to="/experience">{Experience}</Link>
+            <Link to="/">Home</Link>
+            <Link to='/project'>Projects</Link>
+            <Link to="/experience">Experience</Link>
 
         </div>
     </div>
