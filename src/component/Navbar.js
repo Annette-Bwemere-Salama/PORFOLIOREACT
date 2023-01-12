821bfceacb6ad10ab956b886277071cda2c0aa8c
Annette Bwemere <bwemereannette@gmail.com>
2023-01-12 10:37:47 +0200

    feat: materialicons

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import "../styles/navbar.css"
+
 function Navbar() {
   return (
     <div className='navbar'>
         <div className='toggleButton'>
-            <button></button>
+            <button> 
+              </button>
         </div>
         <div className='links'>
             <Link to="/">Home</Link>
